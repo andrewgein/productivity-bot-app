c9a94fa4742e5f012d6730a834db6c7166176a19
andrewgein <av.gein1534@gmail.com>
2025-11-10 22:56:05 +0300

    add tasks view

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import '@maxhub/max-ui/dist/styles.css';
-import { MaxUI, Panel, Button } from "@maxhub/max-ui";
+import '@maxhub/max-ui/dist/styles.css'
+import { useState } from 'react';
+import { MaxUI, Panel, Flex, Container, ToolButton } from "@maxhub/max-ui";
+import TasksView from './components/TasksView';
 
 const App = () => {
+    const [activeTab, setActiveTab] = useState(0);
+
     return (
-        <MaxUI>
-            <Panel centeredX centeredY>
-                <Button>
-                    Hello world!
-                </Button>
-            </Panel>
-        </MaxUI>
+        <div className="full-height-child">
+            <MaxUI>
+                <Container>
+                <Panel style={{marginTop: "15px", marginBottom: "15px"}}>
+                    <Flex direction="row" gap={32}>
+                        <ToolButton onClick={() => setActiveTab(0)}>Задачи</ToolButton>
+                        <ToolButton onClick={() => setActiveTab(1)}>Календарь</ToolButton>
+                    </Flex>
+                </Panel>
+
+                    {(activeTab == 0) &&
+                        <TasksView />
+                    }
+                </Container>
+            </MaxUI>
+        </div>
     )
 }
 
