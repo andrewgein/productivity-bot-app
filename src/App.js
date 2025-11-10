96c06d3afc91aabce0a9bd1cee5e25d8d3e40872
andrewgein <av.gein1534@gmail.com>
2025-11-10 13:24:09 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import '@maxhub/max-ui/dist/styles.css';
 import { MaxUI, Panel, Button } from "@maxhub/max-ui";
 
 const App = () => {
-return (
-  <MaxUI>
-    <Panel centeredX centeredY>
-      <Button>
-        Hello world!
-      </Button>
-    </Panel>
-  </MaxUI>
-)
+    return (
+        <MaxUI>
+            <Panel centeredX centeredY>
+                <Button>
+                    Hello world!
+                </Button>
+            </Panel>
+        </MaxUI>
+    )
+}
 
 export default App;
