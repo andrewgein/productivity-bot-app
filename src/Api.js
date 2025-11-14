58510209f4410544a3646e7956f36ccae192731b
rahmedovdamir <rahmedov17@mail.ru>
2025-11-14 22:02:25 +0300

    for deploy

--- src/Api.js
+++ src/Api.js
@@ -1,19 +1,28 @@
 import axios from 'axios'
 import configData from './config.json';
+import $script from 'scriptjs';
 
 const endpoint = axios.create({ 
     baseURL: configData.API_BASE_URL
 })
+
+let userId = 1
+let body = {}
+$script.get('https://st.max.ru/js/max-web-app.js', function() {
+	debugger;
+	if (window.WebApp.initData) {
+		userId = window.WebApp.initDataUnsafe.user.id
+	}
+})
+
 export default {
     async getTasks() {
-        const maxId = window.WebAppData.user.id || 87852389
-        const result = await endpoint.get(`/get-tasks/${maxId}`)
+        const result = await endpoint.get(`/get-tasks/${userId}` + JSON.stringify(body, null, 2))
         return result.data
     },
 
     async addTask(task) {
-        const maxId = window.WebAppData.user.id || 87852389
-        const result = await endpoint.post("/add-task", { ...task,  userId: maxId});
+        const result = await endpoint.post("/add-task", { ...task,  userId: userId});
         return result.data;
     },
 
